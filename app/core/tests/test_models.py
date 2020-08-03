00fed28902e26e0cf2c5e31cd5a089d9d49fd569
reza <bakhshi.r@gmail.com>
2020-08-03 23:16:05 +0430

    Added Recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,15 +1,21 @@
+from unittest.mock import patch
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 from core import models
 
 
-def sample_user(email='test222@londonappdev.com', password='testpass'):
+def sample_user(email='test226@londonappdev.com', password='testpass'):
     """Create a sample user"""
     return get_user_model().objects.create_user(email, password)
 
 
-def sample_user2(email='test223@londonappdev.com', password='testpass'):
+def sample_user2(email='test227@londonappdev.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
+
+def sample_user3(email='test228@londonappdev.com', password='testpass'):
     """Create a sample user"""
     return get_user_model().objects.create_user(email, password)
 
@@ -66,3 +72,24 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+        def test_recipe_str(self):
+            """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user3(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
+        # @patch('uuid.uuid4')
+        # def test_recipe_file_name_uuid(self, mock_uuid):
+        #     """Test that image is saved in the correct location"""
+        # uuid = 'test-uuid'
+        # mock_uuid.return_value = uuid
+        # file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+
+        # exp_path = f'uploads/recipe/{uuid}.jpg'
+        # self.assertEqual(file_path, exp_path)
